172de160a02be401ad77eb43b4cb74717cee73d5
yuda <33281225+yudaprama@users.noreply.github.com>
2019-04-30 21:47:07 +0700

    import css and font

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,10 +10,10 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./orange.css"
-import './bootstrap.min.css'
-import './font-awesome.min.css'
-import './magnific-popup.css'
+import "./css/orange.css"
+import './css/bootstrap.min.css'
+import './css/font-awesome.min.css'
+import './css/magnific-popup.css'
 
 const Layout = ({ children }) => (
   <StaticQuery
