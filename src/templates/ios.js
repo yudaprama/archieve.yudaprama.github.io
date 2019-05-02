c7321e0309a7837c7273865ac56afa7bcfa39416
yuda <33281225+yudaprama@users.noreply.github.com>
2019-05-03 05:01:20 +0700

    fix ios technicalHighlight.highlights

--- src/templates/ios.js
+++ src/templates/ios.js
@@ -4,9 +4,28 @@ import DetailApp from "../components/detail-app"
 
 export default ({ data }) => {
   const {ios, technicalHighlight} = data;
+  let highlights;
+  if (technicalHighlight)
+    highlights = technicalHighlight.highlights;
+  else
+    highlights = [
+      "Monetization using admob",
+      "Monetization using In App Purchase",
+      "Google search",
+      "Offline browsing",
+      "Organize browsing bookmark",
+      "Organize search result",
+      "Offline reading",
+      "Organize reading bookmark",
+      "4 themes for reading: light, night, sepia, grey",
+      "Change font",
+      "Change text size",
+      "Open in-app browser to see full web page",
+      "In-App Support"
+    ];
   return (
     <DetailApp
-      highlights={technicalHighlight.highlights}
+      highlights={highlights}
       description={ios.description.replace(/\n/g,'<br/>')}
       icon={ios.icon}
       screenShotUrls={ios.screenShotUrls}
