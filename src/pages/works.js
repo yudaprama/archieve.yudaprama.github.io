d64548f64de82456a3f633e1e5ddd56ee46c2422
yuda <33281225+yudaprama@users.noreply.github.com>
2019-05-04 11:54:47 +0700

    cleanup code

--- src/pages/works.js
+++ src/pages/works.js
@@ -83,11 +83,11 @@ function AppItem({id, size, title, icon, summary, price, category}) {
 }
 
 function bytesToSize(bytes) {
-  var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-  if (bytes == 0) return '0 Byte';
-  var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+  let sizes = ["Bytes", "KB", "MB", "GB", "TB"]
+  if (bytes === 0) return '0 Byte';
+  let i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
   return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
-};
+}
 
 export const query = graphql`
 query {
